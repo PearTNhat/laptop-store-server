192a7a51471dffaa0c333fc0be120d748f6cc925
Pear TuanN <letuannhat105@gmail.com>
2025-01-05 16:38:44 +0700

    add cros local

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 import { connectDB } from './configs/mongodb';
 import initRoutes from './routes';
 const corsOptions = {
-  origin: [process.env.BASE_URL_FRONTEND],
+  origin: [process.env.BASE_URL_FRONTEND,'http://localhost:5173'],
   credentials: true,
   optionSuccessStatus: 200,
 }
